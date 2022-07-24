6879d34c885bab2084b7da2f6d846622de09b884
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-24 18:20:39 +0300

    add task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
-import Throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const LOKAL_KEY = 'videoplayer-current-time';
+const LOCAL_KEY = 'videoplayer-current-time';
 
-player.on('timeupdate', Throttle(onPlay, 1000));
+player.on('timeupdate', throttle(onPlay, 1000));
 function onPlay(ev) {
-  localStorage.setItem(LOKAL_KEY, ev.seconds);
+  localStorage.setItem(LOCAL_KEY, ev.seconds);
 }
 
 setCurrentTime();
 function setCurrentTime() {
-  const savedTime = localStorage.getItem(LOKAL_KEY);
+  const savedTime = localStorage.getItem(LOCAL_KEY);
   if (savedTime) {
     player.setCurrentTime(savedTime);
   }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
 
+form.addEventListener('input', throttle(onInput, 500));
+
+form.addEventListener('submit', onSubmit);
+
+const formData = localStorage.getItem(LOCAL_KEY)
+  ? JSON.parse(localStorage.getItem(LOCAL_KEY))
+  : {};
+
+function onInput({ target }) {
+  formData[target.name] = target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+}
+
+function onSubmit(ev) {
+  ev.preventDefault();
+  const keyParsed = JSON.parse(localStorage.getItem(LOCAL_KEY));
+  const email = ev.currentTarget.elements.email.value;
+  const message = ev.currentTarget.elements.message.value;
+  if (email === '' || message === '') {
+    return alert('Всі поля мають бути заповнені');
+  }
+  console.log(keyParsed);
+  ev.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
+}
+function setFormValues() {
+  const sevedInfo = localStorage.getItem(LOCAL_KEY);
+
+  if (sevedInfo) {
+    const data = JSON.parse(sevedInfo);
+    if (data.email) {
+      form.elements.email.value = data.email;
+    }
+    if (data.email) {
+      form.elements.message.value = data.message;
+    }
+  }
+}
+setFormValues();
